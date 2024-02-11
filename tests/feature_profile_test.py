4cc5d56ca7471b70d308e18d7b55a186b1ce7848
Rustambek-dot <127026907+Rustambek-dot@users.noreply.github.com>
2024-02-11 14:45:17 +0300

    Slacki chat2

--- tests/feature_profile_test.py
+++ tests/feature_profile_test.py
@@ -2,7 +2,7 @@ import random
 import allure
 import pytest
 from base.base_test import BaseTest
-@pytest.mark.good
+
 @allure.feature("Profile Functionality")
 class TestProfileFeature(BaseTest):
 
@@ -22,6 +22,3 @@ class TestProfileFeature(BaseTest):
         self.personal_page.is_changes_saved()
         self.personal_page.make_screenshot("Success")
 
-@pytest.mark.bad
-def test_two():
-    assert 1 == 2
