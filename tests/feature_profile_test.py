f543ad28282842cfe0415808ee23a029a2bb234d
Rustambek-dot <127026907+Rustambek-dot@users.noreply.github.com>
2024-02-12 19:50:53 +0300

    Testing Marks2

--- tests/feature_profile_test.py
+++ tests/feature_profile_test.py
@@ -13,7 +13,7 @@ class TestProfileFeature(BaseTest):
         self.login_page.open()
         self.login_page.enter_login(self.data.LOGIN)
         self.login_page.enter_password(self.data.PASSWORD)
-        self.login_page.click_submit_button()
+
         self.dashboard_page.is_opened()
         self.dashboard_page.click_my_info_link()
         self.personal_page.is_opened()
