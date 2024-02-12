b6ec9c844ee9408076c1c71be4225e45f24ea06e
Rustambek-dot <127026907+Rustambek-dot@users.noreply.github.com>
2024-02-12 19:41:37 +0300

    Testing Marks

--- tests/feature_profile_test.py
+++ tests/feature_profile_test.py
@@ -12,7 +12,7 @@ class TestProfileFeature(BaseTest):
     def test_change_profile_name(self):
         self.login_page.open()
         self.login_page.enter_login(self.data.LOGIN)
-        self.login_page.enter_password(self.data.PASSWORD)
+        self.login_page.enter_password(self.data.)
         self.login_page.click_submit_button()
         self.dashboard_page.is_opened()
         self.dashboard_page.click_my_info_link()
